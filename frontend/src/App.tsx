501d95324f2022c70a2c1b0098747dc1d2fc29e4
Trần Nguyễn Anh Khoa <trannguyenanhkhoa0104@gmail.com>
2025-03-18 16:49:40 +0700

    Feat(frontend): #5 - khoahotran create sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Routes, Route } from "react-router-dom"
 import { ThemeProvider } from "./components/ThemeProvider"; // Import từ file vừa tạo
-import { Children } from "react";
+import { Sidebar } from "lucide-react";
 
 function App(): JSX.Element {
   return (
     <>
       <ThemeProvider>
-        <Router>
-          <Routes>
-              <Route path="/" element={Children} />
-
-          </Routes>
-        </Router>
+        <Routes>
+            <Route path="/" element={<Sidebar />} />
+        </Routes>
       </ThemeProvider>
     </>
   )
