8a59516152aa34a56869f602158548e27f5ff2d7
Trần Nguyễn Anh Khoa <trannguyenanhkhoa0104@gmail.com>
2025-03-27 10:24:39 +0700

    Fix(backend): khoahotran fix status code of login

--- backend/dist/user/user.controller.d.ts
+++ backend/dist/user/user.controller.d.ts
@@ -6,8 +6,6 @@ export declare class UserController {
     createUser(response: Response, username: string, email: string, password: string): Promise<{
         message: string;
     }>;
-    loginUser(response: Response, email: string, password: string): Promise<{
-        message: string;
-    }>;
+    loginUser(response: Response, email: string, password: string): Promise<Response<any, Record<string, any>>>;
     getUsers(req: Request): Promise<import("../schemas/user.schema").User[]>;
 }
--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
-import { Controller, Post, Body, Res, Req, Get } from '@nestjs/common';
+import { Controller, Post, Body, Res, Req, Get, HttpCode, HttpStatus } from '@nestjs/common';
 import { UserService } from './user.service';
 import { Response, Request } from 'express';
 
@@ -10,6 +10,7 @@ import { Response, Request } from 'express';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @HttpCode(HttpStatus.CREATED)
   @Post('signup')
   async createUser(
     @Res({ passthrough: true }) response: Response,
@@ -29,11 +30,14 @@ export class UserController {
     @Body('password') password: string,
   ) {
     const acc = await this.userService.loginUser(email, password);
-    if (acc.length) {
+    console.log('acc: ', acc);
+    
+    if (acc.length !== 0) {
       response.cookie('token', acc[0]['_id'].toString(), { httpOnly: true, secure: true, maxAge: 1000 * 60 * 60 * 24 * 7 });
-      return { message: 'Login successful' };
+      return response.status(200).json({ message: 'Login successful' });
     } else {
-      return { message: 'Login failed' };
+      console.log('failed');
+      return response.status(401).json({ message: 'Login failed' });
     }
   }
 
